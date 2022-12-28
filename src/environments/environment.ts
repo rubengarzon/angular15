98b0686b05941652db89e176af1c5be035d8b681
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-12-28 19:37:11 +0100

    firebase añadido

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyCV_991wmSEepIdRB97e_itZsAtCAf4Mn8',
+    authDomain: 'angular15-3b66a.firebaseapp.com',
+    projectId: 'angular15-3b66a',
+    storageBucket: 'angular15-3b66a.appspot.com',
+    messagingSenderId: '1086320504233',
+    appId: '1:1086320504233:web:acd188ec81f80e589928b8',
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { importProvidersFrom } from '@angular/core';
+import { environment } from './environments/environment';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideFirestore(() => getFirestore())
+    ),
+  ],
+});
