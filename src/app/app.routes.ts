651c46169d0124ccbd3211935d3ed6ae521a350c
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-12-29 12:01:15 +0100

    componentes useredit, useradd, userlist creados

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+
+export const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'users',
+    pathMatch: 'full',
+  },
+  {
+    path: 'users',
+    
+  }
+];
